750f881d06cefc458d092fbbb5fc458e748548f3
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-08-31 18:37:44 +0200

    Can convert video into mp3

--- ytmp3.py
+++ ytmp3.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 from sys import argv
+from moviepy.editor import *
 
 link = argv[1]
 yt = YouTube(link)
@@ -11,3 +12,12 @@ print("Views: ", yt.views)
 yd = yt.streams.get_highest_resolution()
 
 yd.download("D:/")
+
+mp4_file = r'D:/3 PYTHON AUTOMATION PROJECTS FOR BEGINNERS.mp4'
+mp3_file = r'D:/3 PYTHON AUTOMATION PROJECTS FOR BEGINNERS.mp3'
+
+videoclip = VideoFileClip(mp4_file)
+audioclip = videoclip.audio
+audioclip.write_audiofile(mp3_file)
+audioclip.close()
+videoclip.close()
