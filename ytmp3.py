8bbfeccb65d32d5bf16f9cb75d56eb91b8e91805
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-01 08:54:00 +0200

    Added window to paste URL

--- ytmp3.py
+++ ytmp3.py
@@ -7,9 +7,28 @@ from pytube import YouTube
 import os
 
 root = tk.Tk()
+apps = []
 
 def addFolder():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = filedialog.askopenfilename(initialdir="/", title="Select Folder", filetypes=(("executables", ".exe"), ("all files", "*.*")))
+    apps.append(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+def runApp():
+    link = pasteUrl.get()
+    yt = YouTube(link)
+    yd = yt.streams.filter(only_audio=True).first()
+    out = yd.download("D:/")
+
+    base, ext = os.path.splitext(out)
+    new_file = base + '.mp3'
+    os.rename(out, new_file)
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
@@ -17,31 +36,22 @@ canvas.pack()
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
+text = Text(root)
+text.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+text.insert("1.0", "Paste URL here:")
+text['state'] = 'disabled'
+
+pasteUrl = tk.Entry(root)
+canvas.create_window(235, 110, width=300, window=pasteUrl)
+
 openFolder = tk.Button(root, text="Open folder", padx=10, pady=5, fg="white", bg="#263D42", command=addFolder)
 
 openFolder.pack()
 
-runApp = tk.Button(root, text="Run App", padx=10, pady=5, fg="white", bg="#263D42")
+runApp = tk.Button(root, text="Run App", padx=10, pady=5, fg="white", bg="#263D42", command=runApp)
 
 runApp.pack()
 
 root.mainloop()
-'''
-link = input("Paste URL here: ")
-yt = YouTube(link)
 
-print("Title: ", yt.title)
-
-yd = yt.streams.filter(only_audio=True).first()
-out = yd.download("D:/")
-
-base, ext = os.path.splitext(out)
-new_file = base + '.mp3'
-os.rename(out, new_file)
-
-print(yt.title + " saved")
-
-input("Press anything to exit...")
-
-'''
 
